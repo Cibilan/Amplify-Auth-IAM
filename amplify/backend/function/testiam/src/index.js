3bce64c95521871052ee8e0a6029fe6fe7d1ae2f
Cibilan <cibilan@gmail.com>
2020-07-02 11:49:34 +0530

    API with IAM access and hello world Lambda

---
+++ amplify/backend/function/testiam/src/index.js
@@ -0,0 +1,15 @@
+/* Amplify Params - DO NOT EDIT
+	API_AMPLIFYGQL_GRAPHQLAPIENDPOINTOUTPUT
+	API_AMPLIFYGQL_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
